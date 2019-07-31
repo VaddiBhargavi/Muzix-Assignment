b9be48e5b9ed057736b43ef50f859d54f6a0a258
VaddiBhargavi <51950184+VaddiBhargavi@users.noreply.github.com>
2019-07-31 09:13:27 +0530

    Update Result.java

--- src/main/java/com/stackroute/MuzixAssignment/MuzixAppMysql/domain/Result.java
+++ src/main/java/com/stackroute/MuzixAssignment/MuzixAppMysql/domain/Result.java
@@ -3,6 +3,8 @@ package com.stackroute.MuzixAssignment.MuzixAppMysql.domain;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+
+//Result class has a dependency on TrackMatches class
 public class Result {
     public TrackMatches results;
 }
